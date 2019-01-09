b44310076908d1ee9dcf863e1b41d52a86056e51
Snehith Challa <challasnehith@gmail.com>
2019-01-08 23:26:11 -0500

    Create letVsVar
    A few differences between let and var

---
+++ letVsVar.js
@@ -0,0 +1,3 @@
+// let vs const 
+// let allows you to declare variables that are limited in scope to the block, statement, or expression on which it is 
+// used. This is unlike the var keyword, which defines a variable globally, or locally to an entire function regardless of block s // cope. An explanation of why the name "let" was chosen can be found here.
