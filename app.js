bfb845008ca4ee6b652bd117a3c05cdce43bcb3a
Snehith Challa <challasnehith@gmail.com>
2019-01-11 16:49:58 -0500

    Update app.js
    Classes are added. Event listeners yet to be added

--- app.js
+++ app.js
@@ -1,72 +1,98 @@
-let app = {
-	name: 'Indecision App',
-	subtitle: 'Put your life in hands of a computer',
-	options: []
-};
-
-
-
-let count = 0;
-
-let addOne = () => {
-	let change = document.getElementById('add');
-	count += 1;
-	change.innerHTML = count;
-	console.log(change.innerHTML);
-	renderApp();
-}
-
-let reset = () => {
-	let change = document.getElementById('add');
-	count = 0;
-	change.innerHTML = count;
-	app.options = [];
-	renderApp();
-}
-
-let submitted = (e) => { 
-	e.preventDefault();
-	let value = e.target.elements.option.value;
-	if(value){
-		e.target.elements.option.value = ' ';
-		app.options.push(value);
-		addOne();
-		renderApp();
+class IndecisionApp extends React.Component {
+	render(){
+		const title = 'Indecision';
+		const subTitle = 'Put your life in hands of a computer';
+		const opt = ['thing 1', 'thing 2', 'thing 3'];
+		return (
+			<div>
+				<Header title = {title} subTitle = {subTitle}/>
+				<Action />
+				<Options opt = {opt}/>
+				<AddOption />
+			</div>
+		);
 	}
 }
 
-// used for keys of child in react. with the help of these keys react can reference efficiently without
-// having to render all the objects in array.
-// only the object with specific key can be changed.
-let key = 0;
-
-let renderApp = () => {
-
-	let templateThree = (
-		<div>
-			<h1> Indecision App </h1>
-			<p> Put your life in hands of a computer </p>
-			<p> {app.options.length > 0 ? 'Here are your options' : 'No options'} </p>
-			<h1 id='add'> {count} </h1>
-			<button onClick = {reset} > Remove All </button>
-			<ol> 
-				{
-					app.options.map((number) => <li key = {key++}> {number} </li>)
-				}		
-			</ol>
-			
-			<form onSubmit = {submitted}>
-				<input name = "option" type = 'text'/> 
-				<button> Add Option </button> 
-			</form>
-		</div>
-	);
-
-
-	var appRoot = document.getElementById('app');
-
-	ReactDOM.render(templateThree, appRoot);
-
+class Header extends React.Component {
+	render(){
+		return (
+			<div>
+				<h1> {this.props.title} </h1>
+				<h2> {this.props.subTitle}  </h2>
+			</div>
+		);
+	}
 }
 
-renderApp();
+
+class Action extends React.Component {
+	render(){
+		return (
+			<div> 
+
+				<button> What should I do </button>
+
+			</div>
+		);
+
+	}
+}
+
+class Options extends React.Component {
+	handleRemove(){
+		
+	}
+	render(){
+		return (
+			<div>
+				<button onClick = {this.handleRemove}> Remove All </button>
+				{
+
+					this.props.opt.map((options) => {
+
+						return <Option key = {options} text = {options}> </Option>
+
+					})  
+				}
+			</div>	
+		);
+	}
+}
+
+class Option extends React.Component {
+	render(){
+		return (
+			<div> 
+				{
+					this.props.text
+				}
+			</div>
+		);
+
+	}
+}
+
+
+class AddOption extends React.Component {
+	handleAddOption(e){
+		e.preventDefault();
+		const option = e.target.elements.option.value.trim;
+		if(option)
+			alert(option);
+	}
+	render(){
+		return (
+			<div> 
+				<form onSubmit = {this.handleAddOption}> 
+					<input type = 'text' name = 'option'/>
+					<button> Add Option </button>
+				</form>
+			</div>
+			
+		);
+
+	}
+}
+
+ReactDOM.render(<IndecisionApp/>,document.getElementById('app'));
