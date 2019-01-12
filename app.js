c5e052d120d5bc067ee52db72bb62fed889bc6f6
Snehith Challa <challasnehith@gmail.com>
2019-01-12 06:23:53 -0500

    Update app.js
    Add "Add option" functionality with all the error validatoin

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ class IndecisionApp extends React.Component {
 	}
 
 	handleAddOption(optionNew) {
+	    if (!optionNew) {
+	      return 'Enter valid value to add item';
+	    } else if (this.state.opt.indexOf(optionNew) > -1) {
+	      return 'This option already exists';
+	    }
 		this.setState((prevState) => {
 			return {
 				opt : prevState.opt.concat(optionNew)
@@ -98,15 +103,27 @@ class Option extends React.Component {
 
 
 class AddOption extends React.Component {
+	constructor(props){
+		super(props);
+		this.handleAddOption = this.handleAddOption.bind(this);
+		this.state = {
+			error : undefined
+		}
+	}
 	handleAddOption(e){
 		e.preventDefault();
-		const option = e.target.elements.option.value.trim;
-		if(option)
-			alert(option);
+
+		const option = e.target.elements.option.value.trim();
+		const error = this.props.handleAddOption(option);
+
+	    this.setState(() => {
+	      return { error };
+	    });
 	}
 	render(){
 		return (
 			<div> 
+		        {this.state.error && <p>{this.state.error}</p>}
 				<form onSubmit = {this.handleAddOption}> 
 					<input type = 'text' name = 'option'/>
 					<button> Add Option </button>
