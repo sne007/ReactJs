abd35f2e599cb3bba66bce8d35ca851d9319ee4a
Snehith Challa <challasnehith@gmail.com>
2019-01-11 20:26:33 -0500

    Update app.js
    HandlePick and diable when options is empty

--- app.js
+++ app.js
@@ -13,6 +13,38 @@ class IndecisionApp extends React.Component {
 		);
 	}
 }
+  handleDeleteOptions() {
+    this.setState(() => {
+      return {
+        options: []
+      };
+    });
+  }
+  handlePick() {
+    const randomNum = Math.floor(Math.random() * this.state.options.length);
+    const option = this.state.options[randomNum];
+    alert(option);
+  }
+  render() {
+    const title = 'Indecision';
+    const subtitle = 'Put your life in the hands of a computer';
+
+    return (
+      <div>
+        <Header title={title} subtitle={subtitle} />
+        <Action
+          hasOptions={this.state.options.length > 0}
+          handlePick={this.handlePick}
+        />
+        <Options
+          options={this.state.options}
+          handleDeleteOptions={this.handleDeleteOptions}
+        />
+        <AddOption />
+      </div>
+    );
+  }
+}
 
 class Header extends React.Component {
 	render(){
