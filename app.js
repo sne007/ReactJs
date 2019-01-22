30b4dce928933d2cb311b0acd44646fe85de1e42
Snehith Challa <challasnehith@gmail.com>
2019-01-21 23:18:09 -0500

    Update app.js
    Update with integration of third party dependencies

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-// import './utils.js';
-// import { square, add } from './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(100, 23));
-
-import { isAdult, canDrink } from './person.js';
-console.log(isAdult(18));
-console.log(canDrink(20));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
