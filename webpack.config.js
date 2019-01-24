d9825278b093e1e7cf65759e91e208e224e576ed
Snehith Challa <challasnehith@gmail.com>
2019-01-23 23:51:26 -0500

    Update webpack-config.js

--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,18 @@ module.exports = {
       loader: 'babel-loader',
       test: /\.js$/,
       exclude: /node_modules/
+    }, {
+      test: /\.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
     }]
   },
   devtool: 'cheap-module-eval-source-map',
   devServer: {
-    contentBase: path.join(__dirname, 'public')
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true
   }
 };
